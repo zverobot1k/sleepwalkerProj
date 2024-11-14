e225bb21179f326dd8e1953374a25cfaebc9c190
Максим Блохин <maksimblohin@MacBook-Air-Maksim.local>
2024-11-14 16:50:09 +0300

    sum func

--- sleepproj.go
+++ sleepproj.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+// func main() {
+// 	numbers := []int{10, -9, -40, 35, 90}
+// 	fmt.Println("Массив до сортировки:", numbers)
+// 	sort(numbers)
+// 	fmt.Println("Массив после сортировки по абсолютным значениям:", numbers)
+// }
+
+// func sort(arr []int) {
+// 	for i := 0; i < (len(arr) - 1); i++ {
+// 		if math.Abs(float64(arr[i])) > math.Abs(float64(arr[i+1])) {
+// 			arr[i], arr[i+1] = arr[i+1], arr[i]
+// 		}
+// 	}
+// }
+
 func main() {
-	numbers := []int{10, -9, -40, 35, 90}
-	fmt.Println("Массив до сортировки:", numbers)
-	sort(numbers)
-	fmt.Println("Массив после сортировки по абсолютным значениям:", numbers)
+	var a int
+	var b int
+	fmt.Scan(&a)
+	fmt.Scan(&b)
 }
 
-func sort(arr []int) {
-	for i := 0; i < (len(arr) - 1); i++ {
-		if math.Abs(float64(arr[i])) > math.Abs(float64(arr[i+1])) {
-			arr[i], arr[i+1] = arr[i+1], arr[i]
-		}
-	}
+func summ(a, b int) int {
+	return a + b
 }
